2a0161f4b9999ecaaada71c21846033893df9716
Andrey <andreymail98@gmail.com>
2020-08-15 11:58:28 +0300

    server init and app.js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/frontendchallenge_db');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 8081)
